83fa96250401055e9b61abc136f8b0bfb14d6b5d
Mina Sameh <minasamehlabib@gmail.com>
2017-05-24 16:51:06 +0200

    Main view UI step 1

--- app/src/main/java/mina/apps/countries/Main.java
+++ app/src/main/java/mina/apps/countries/Main.java
@@ -1,7 +1,26 @@
 package mina.apps.countries;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.ExpandedMenuView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Main extends AppCompatActivity {
 
